09eaa69747c0c5294b99d36abc839dc122eee5fe
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-27 03:53:41 +0530

    Basic ping app

---
+++ flights/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+// Create express app and mw
+const app = express();
+app.use(express.json());
+
+// Routes
+app.get("/ping", (_req, res) => res.send("pong"));
+
+// Start listening
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
