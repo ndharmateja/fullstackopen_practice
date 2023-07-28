168baf48e354dd9751c481165406b5bb13544d30
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-29 03:49:06 +0530

    Install and use morgan

--- flights/src/index.ts
+++ flights/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import diariesRouter from "./routes/diaries";
+import morgan from "morgan";
 
 // Create express app and mw
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 // Routes
 app.get("/ping", (_req, res) => res.send("pong"));
