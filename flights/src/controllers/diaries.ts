1a4774261b48f919e13123534eb70d11d2462e53
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-28 13:14:33 +0530

    Add controller to get diary by id

--- flights/src/controllers/diaries.ts
+++ flights/src/controllers/diaries.ts
@@ -5,6 +5,12 @@ export const getDiaries = (_req: express.Request, res: express.Response) => {
   return res.json(diaryService.getNonSensitiveDiaryEntries());
 };
 
+export const getDiary = (req: express.Request, res: express.Response) => {
+  const id = Number(req.params.id);
+  const entry = diaryService.getEntryById(id);
+  return entry ? res.json(entry) : res.sendStatus(404);
+};
+
 export const postDiaries = (_req: express.Request, res: express.Response) => {
   return res.send("add new diary");
 };
