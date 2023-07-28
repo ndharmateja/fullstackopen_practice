16f5cfde136edf5d980e5ec80011d34b5bc36985
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-28 12:40:27 +0530

    get non sensitive entries in get controller

--- flights/src/controllers/diaries.ts
+++ flights/src/controllers/diaries.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import diaryService from "../services/diaryService";
 
 export const getDiaries = (_req: express.Request, res: express.Response) => {
-  return res.send("get all diaries");
+  return res.json(diaryService.getNonSensitiveDiaryEntries());
 };
 
 export const postDiaries = (_req: express.Request, res: express.Response) => {
