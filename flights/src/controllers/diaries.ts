0d4806db1250c31f318f54938878ac4310743550
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-29 04:50:50 +0530

    Update post diaries controller

--- flights/src/controllers/diaries.ts
+++ flights/src/controllers/diaries.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import diaryService from "../services/diary_service";
+import { toNewDiaryEntry } from "../utils";
 
 export const getDiaries = (_req: express.Request, res: express.Response) => {
   return res.json(diaryService.getNonSensitiveDiaryEntries());
@@ -11,6 +12,8 @@ export const getDiary = (req: express.Request, res: express.Response) => {
   return entry ? res.json(entry) : res.sendStatus(404);
 };
 
-export const postDiaries = (_req: express.Request, res: express.Response) => {
-  return res.send("add new diary");
+export const postDiaries = (req: express.Request, res: express.Response) => {
+  const newDiary = toNewDiaryEntry(req.body);
+  const diaryEntry = diaryService.addEntry(newDiary);
+  return res.json(diaryEntry);
 };
