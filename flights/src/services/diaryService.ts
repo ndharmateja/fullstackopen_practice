8d4e9814083e2130bf3f1027f637aba0b6ea45dc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-28 12:40:38 +0530

    Add fun to get non sensitive entries

--- flights/src/services/diaryService.ts
+++ flights/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from "../data/entries";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NonSensitiveDiaryEntry } from "../types";
 
 const diaryData: DiaryEntry[] = diaries;
 
@@ -7,8 +7,17 @@ const getEntries = (): DiaryEntry[] => {
   return diaryData;
 };
 
+const getNonSensitiveDiaryEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaryData.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
 const addEntry = () => {
   return null;
 };
 
-export default { getEntries, addEntry };
+export default { getEntries, addEntry, getNonSensitiveDiaryEntries };
